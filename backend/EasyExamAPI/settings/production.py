83c2b2b648514cc3bc9861de0e2e996d75984005
Joaquín Cruz <joacocruz6@gmail.com>
2020-04-30 11:57:28 -0400

    chore(settings): Rearrange some settings and update gitignore

---
+++ backend/EasyExamAPI/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = False
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": get_env_variable("DATABASE_NAME"),
+        "USER": get_env_variable("DATABASE_USER"),
+        "PASSWORD": get_env_variable("DATABASE_PASSWORD"),
+        "HOST": "",
+        "PORT": "",  # TODO: Complete with the data on production
+    }
+}
