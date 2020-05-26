3eb9cfcefcde394585ad1f2cc125d577183f0db5
Joaquin Cruz Doggenweiler <joaquin.cruz@ug.uchile.cl>
2020-05-25 22:32:07 -0400

    Revert "feat(user): Refactor user model to create what we need"

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -1,30 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from apps.exam.storage import OverwriteStorage
-from django.conf import settings
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True, blank=False)
-    updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(
-        upload_to=settings.USER_IMAGE_PATH,
-        storage=OverwriteStorage(),
-        blank=True,
-        null=True,
-    )
-
-    @property
-    def is_admin(self):
-        return self.is_superuser
-
-    @property
-    def fullname(self):
-        return f"{self.first_name} {self.last_name}"
-
-    @property
-    def created_at(self):
-        return self.date_joined
+    email = models.EmailField(blank=False)
 
 
 class Transaction(models.Model):
