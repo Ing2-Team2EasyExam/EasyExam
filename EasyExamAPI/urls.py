2f5d13ead40283a2652f9e120f3dc44ab5915bd2
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-02 13:25:53 -0400

    feat(api): Put api as standard urls

--- EasyExamAPI/urls.py
+++ EasyExamAPI/urls.py
@@ -21,8 +21,7 @@ from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/", include("apps.user.urls")),
-    path("api/", include("apps.exam.urls")),
+    path("api/", include("apps.api.urls")),
     path("", include("frontend.urls")),
     path("docs/", include_docs_urls(title="Easy Exam API")),
 ]
