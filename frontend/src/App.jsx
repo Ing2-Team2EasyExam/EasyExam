b06b2d53c7f499e6aeae548aa020c77be0507303
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-01 20:33:33 -0400

    feat(frontend): Create hello world app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
