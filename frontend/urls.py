1184c1b7c876113344a2629a79d425a717f8b736
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-04 11:04:21 -0400

    chore(views): Change frontend view of the backend

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.FrontendIndexView.as_view(), name="frontend-index"),
-    path("logged_in/", views.FrontendIndexView.as_view(), name="fronted-logged-in"),
+    path("", views.FrontEndRendererView.as_view(), name="frontend-index"),
+    path("logged_in/", views.FrontEndRendererView.as_view(), name="fronted-logged-in"),
 ]
