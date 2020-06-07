a9308789c55da1df1d617914e4ab699c22bd7569
Erick Lemus <elemusf@gmail.com>
2020-06-07 17:17:10 -0400

    feat(frontend): Create Problema Component

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,11 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.FrontEndRendererView.as_view(), name="frontend-index"),
-<<<<<<< HEAD
     path("logged_in/", views.FrontEndRendererView.as_view(), name="frontend-logged-in"),
     path("home/", views.FrontEndRendererView.as_view(), name="frontend-home"),
-=======
-    path("logged_in/", views.FrontEndRendererView.as_view(), name="fronted-logged-in"),
     path("problems/", views.FrontEndRendererView.as_view(), name="frontend-problem-list"),
->>>>>>> feat(frontend): Create Problema Component
 ]
