7211a6fbc9cd7b9a2d88a79bc33f5e0d05a7250d
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-12 00:43:57 -0400

    feat(services): Create service to create or update an exam

--- apps/exam/services.py
+++ apps/exam/services.py
@@ -2,7 +2,47 @@ from .models import Problem, Exam, Topic
 from typing import Set, Tuple, List
 
 
-def get_problem_topics(problem):
+def create_or_update_exam(**data) -> Exam:
+    name = data["name"]
+    owner = data["owner"]
+    teacher = data["teacher"]
+    university = data["university"]
+    course_name = data["course_name"]
+    course_code = data["course_code"]
+    language = data["language"]
+    problems = data["problems"]
+    due_date = data["due_date"]
+    start_time = data["start_time"]
+    end_time = data["end_time"]
+
+    exam, created = Exam.objects.update_or_create(
+        name=name,
+        owner=owner,
+        teacher=teacher,
+        university=university,
+        course_name=course_name,
+        course_code=course_code,
+        language=language,
+        due_date=dute_date,
+        start_date=start_date,
+        end_time=end_time,
+        problems=problems,
+    )
+    try:
+        exam.generate_pdf()
+    except CompilationErrorException as err:
+        if created:
+            exam.delete()
+        raise ValidationError(err.latex_logs)
+    except Exception:
+        if created:
+            exam.delete()
+        raise ValidationError(
+            "There was an internal error in the compilation of the latex file"
+        )
+
+
+def get_problem_topics(problem: Problem) -> Set[str]:
     """
     Returns a set with the problem's topics names
     :param problem: An instance of the model Problem
@@ -14,7 +54,7 @@ def get_problem_topics(problem):
     return topics
 
 
-def get_exam_topics(exam):
+def get_exam_topics(exam: Exam) -> Set[str]:
     """
     Returns a set with the exam's topics, corresponds to the union of the problems topics of the exam
     :param exam: An instance of the model Exam
