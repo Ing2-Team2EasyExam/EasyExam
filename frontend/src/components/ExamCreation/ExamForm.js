2e06081347ec3fefe98b4748017681a5c49ff367
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-14 16:40:49 -0400

    feat(frontend): Add submit button to the create exam form

--- frontend/src/components/ExamCreation/ExamForm.js
+++ frontend/src/components/ExamCreation/ExamForm.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button, Col } from "react-bootstrap";
 
 class ExamForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    alert("Form Submitted");
+  }
   render() {
     const style = {
       // borderRadius: "25px",
@@ -10,49 +18,62 @@ class ExamForm extends React.Component {
     };
     return (
       <>
-        <Form style={style}>
-          <Form.Group controlId="formExamName">
-            <Form.Label>Nombre del Examen</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Ingresar Nombre del Examen"
-            />
-          </Form.Group>
-          <Form.Group controlId="dueDate">
-            <Form.Label>Fecha de reailización</Form.Label>
-            <Form.Control type="date" placeholder="dd/mm/aaaa" />
-          </Form.Group>
-          <Form.Group controlId="startTime">
-            <Form.Label>Hora de Inicio</Form.Label>
-            <Form.Control type="time" placeholder="HH:MM" />
-          </Form.Group>
-          <Form.Group controlId="endTime">
-            <Form.Label>Hora de Termino</Form.Label>
-            <Form.Control type="time" placeholder="HH:MM" />
-          </Form.Group>
-          <Form.Group controlId="teacher">
-            <Form.Label>Nombre profesor/a</Form.Label>
-            <Form.Control type="text" placeholder="Nombre del profesor/a" />
-          </Form.Group>
-          <Form.Group controlId="courseName">
-            <Form.Label>Nombre del curso</Form.Label>
-            <Form.Control type="text" placeholder="Nombre del curso" />
-          </Form.Group>
-          <Form.Group controlId="courseCode">
-            <Form.Label>Codigo del Curso</Form.Label>
-            <Form.Control type="text" placeholder="Codigo del curso" />
-          </Form.Group>
-          <Form.Group controlId="university">
-            <Form.Label>Universidad</Form.Label>
-            <Form.Control type="text" placeholder="Nombre de la Universidad" />
-          </Form.Group>
-          <Form.Group controlId="language">
-            <Form.Label>Idioma del examen</Form.Label>
-            <Form.Control as="select">
-              <option>Ingles</option>
-              <option>Español</option>
-            </Form.Control>
-          </Form.Group>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Row style={{ paddingBottom: "1%" }}>
+            <Col></Col>
+            <Col>
+              <Button variant="success" type="submit">
+                Guardar
+              </Button>
+            </Col>
+          </Form.Row>
+          <div style={style}>
+            <Form.Group controlId="formExamName">
+              <Form.Label>Nombre del Examen</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingresar Nombre del Examen"
+              />
+            </Form.Group>
+            <Form.Group controlId="dueDate">
+              <Form.Label>Fecha de realización</Form.Label>
+              <Form.Control type="date" placeholder="dd/mm/aaaa" />
+            </Form.Group>
+            <Form.Group controlId="startTime">
+              <Form.Label>Hora de Inicio</Form.Label>
+              <Form.Control type="time" placeholder="HH:MM" />
+            </Form.Group>
+            <Form.Group controlId="endTime">
+              <Form.Label>Hora de Termino</Form.Label>
+              <Form.Control type="time" placeholder="HH:MM" />
+            </Form.Group>
+            <Form.Group controlId="teacher">
+              <Form.Label>Nombre profesor/a</Form.Label>
+              <Form.Control type="text" placeholder="Nombre del profesor/a" />
+            </Form.Group>
+            <Form.Group controlId="courseName">
+              <Form.Label>Nombre del curso</Form.Label>
+              <Form.Control type="text" placeholder="Nombre del curso" />
+            </Form.Group>
+            <Form.Group controlId="courseCode">
+              <Form.Label>Codigo del Curso</Form.Label>
+              <Form.Control type="text" placeholder="Codigo del curso" />
+            </Form.Group>
+            <Form.Group controlId="university">
+              <Form.Label>Universidad</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Nombre de la Universidad"
+              />
+            </Form.Group>
+            <Form.Group controlId="language">
+              <Form.Label>Idioma del examen</Form.Label>
+              <Form.Control as="select">
+                <option>Ingles</option>
+                <option>Español</option>
+              </Form.Control>
+            </Form.Group>
+          </div>
         </Form>
       </>
     );
