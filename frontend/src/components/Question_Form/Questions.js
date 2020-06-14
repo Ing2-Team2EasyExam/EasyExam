005b8e832910006d4dc4859e4102663947e48e11
csepulvedaa <mess@LAPTOP-1HNHOLB1.localdomain>
2020-06-13 23:54:32 -0400

    Submit Button , Question name, topics rendering

--- frontend/src/components/Question_Form/Questions.js
+++ frontend/src/components/Question_Form/Questions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container';
 
 class Questions extends React.Component{
@@ -7,52 +8,35 @@ class Questions extends React.Component{
         super(props);
     }
     render(){
-      /*const name=<Form>
-      <Form.Group controlID="Nombres">
-      <Form.Row>
-        <Form.Label column="lg" lg={2}>
-          Nombre Pregunta:
-        </Form.Label>
-        <Col>
-          <Form.Control size="lg" type="text" placeholder="Nombre Pregunta" />
-        </Col>
-      </Form.Row>
-      <br />
-      <Form.Row>
-        <Form.Label column lg={2}>
-          Autor/a:
-        </Form.Label>
-        <Col>
-          <Form.Control type="text" placeholder="Autor/a:" />
-        </Col>
-      </Form.Row>
-      <br />
-      </Form.Group>
-      </Form> */
+      const questionName=<Form.Group controlId="name">
+      <Form.Label>Nombre de la pregunta:</Form.Label>
+      <Form.Control placeholder="Pregunta Induccion 1" />
+    </Form.Group>
 
+      const submit=<Button type="submit" className="my-1">
+      Guardar
+    </Button>
       const topics=
-      <Form>
       <Form.Group controlId="exampleForm.SelectCustomSizeSm">
         <Form.Label>Topicos</Form.Label>
         <Form.Control as="select" size="sm" custom>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+          <option>Topico 1</option>
+          <option>Topico 2</option>
+          <option>Topico 3</option>
+          <option>Topico 4</option>
+          <option>Topico 5</option>
         </Form.Control>
       </Form.Group>
-      </Form>
-      
-            return(
-            <Container style={{marginTop: '100px'}}>
-              
-            {topics}
-        </Container>
-              
-            )
+     return(
+           <Container style={{marginTop: '100px'}}>
+            <Form>
+              {submit}
+              {questionName}
+              {topics}</Form>
+              </Container>
+               )
         
-
+            
     }
 }
 export default Questions;
