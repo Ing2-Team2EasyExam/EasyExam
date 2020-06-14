d4d2e3e1541a0d55b5b382dc3242b1c7eb346c2f
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-14 15:00:07 -0400

    feat(urls): Craete url for the exam creation on the backend

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.FrontEndRendererView.as_view(), name="frontend-index"),
     path("logged_in/", views.FrontEndRendererView.as_view(), name="frontend-logged-in"),
     path("home/", views.FrontEndRendererView.as_view(), name="frontend-home"),
+    path(
+        "exam/create", views.FrontEndRendererView.as_view(), name="frontend-exam-create"
+    ),
 ]
