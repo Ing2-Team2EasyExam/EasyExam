fe2f34cbb55520f929b11d504cddd71f91869b35
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-14 01:08:15 -0400

    test(serializers): Create test for the edit serializer

--- apps/exam/tests/test_serializers/test_exam.py
+++ apps/exam/tests/test_serializers/test_exam.py
@@ -1,6 +1,9 @@
+import datetime
+from unittest import mock
 from django.test import TestCase
 from mixer.backend.django import mixer
 from apps.exam import serializers
+from collections import OrderedDict
 
 
 class TestExamListSerializer(TestCase):
@@ -23,3 +26,48 @@ class TestProblemNestedSerializer(TestCase):
     def test_valid_data(self):
         serializer = serializers.ProblemNestedSerializer(data=self.data)
         self.assertTrue(serializer.is_valid())
+
+
+class TestExamEditSerializer(TestCase):
+    def setUp(self):
+        self.user = mixer.blend("user.User")
+        self.problems = mixer.cycle(2).blend("exam.Problem")
+        self.serialized_problems = [
+            OrderedDict(name=problem.name, author=problem.author)
+            for problem in self.problems
+        ]
+        self.data = {
+            "name": "Control 1",
+            "teacher": "Jeremy Barbay",
+            "university": "U. de Chile",
+            "course_name": "Alg. y estructuras de datos",
+            "course_code": "CC3001",
+            "language": "ES",
+            "due_date": datetime.datetime.now().date(),
+            "start_time": datetime.datetime.now().time(),
+            "end_time": (datetime.datetime.now() + datetime.timedelta(hours=2)).time(),
+            "problems": self.serialized_problems,
+        }
+
+    @mock.patch("apps.exam.models.Exam.generate_pdf")
+    def test_create_exam_from_serializer(self, mock_generate_pdf):
+        request = mock.MagicMock()
+        request.user = self.user
+        context = {"request": request}
+        serializer = serializers.ExamEditSerializer(context=context, data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        exam = serializer.save()
+        self.assertIsNotNone(exam)
+
+    @mock.patch("apps.exam.models.Exam.generate_pdf")
+    def test_update_exam_from_serializer(self, mock_generate_pdf):
+        request = mock.MagicMock()
+        request.user = self.user
+        context = {"request": request}
+        exam = mixer.blend("exam.Exam", problems=self.problems)
+        serializer = serializers.ExamEditSerializer(
+            context=context, instance=exam, data=self.data
+        )
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        exam = serializer.save()
+        self.assertIsNotNone(exam)
