b02db5e30400a25a1325caef23b9040850064637
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-14 15:23:36 -0400

    feat(frontend): Separate the two parts of the page for the creation of the exam

--- frontend/src/components/ExamCreation/CreateExam.js
+++ frontend/src/components/ExamCreation/CreateExam.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-
+import { Container, Row, Col } from "react-bootstrap";
+import ExamForm from "./ExamForm";
+import RenderPDF from "./RenderPDF";
 class CreateExam extends React.Component {
   render() {
-    return <h1>Create exam form</h1>;
+    return (
+      <>
+        <h1>Create Exam</h1>
+        <Container>
+          <Row>
+            <Col>
+              <ExamForm />
+            </Col>
+            <Col>
+              <RenderPDF />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
   }
 }
 
