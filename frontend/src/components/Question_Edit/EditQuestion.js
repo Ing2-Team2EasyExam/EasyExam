270166ffef1cff88026ddc36ed91eb922a516faa
csepulvedaa <mess@LAPTOP-1HNHOLB1.localdomain>
2020-06-20 23:27:37 -0400

    Components created, not rendering yet

---
+++ frontend/src/components/Question_Edit/EditQuestion.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import RenderPDF from "./RenderPDF";
+
+class EditQuestion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    alert("Form Submitted");
+  }
+  render() {
+    return (
+      <>
+        <h1>Editar Pregunta </h1>
+        <Container>
+          <Row>
+            <Col>
+              <QuestionEditForm uuid={uuid} />
+            </Col>
+            <Col>
+              <RenderPDF />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default EditQuestion;
