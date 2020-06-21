314bf51447b1edd690cb6345948583f2915437fb
Joaquín Cruz <joacocruz6@gmail.com>
2020-06-21 18:18:28 -0400

    feat(problems): Add link to the problem previews

--- frontend/src/components/Problem_list/OwnProblem.js
+++ frontend/src/components/Problem_list/OwnProblem.js
@@ -6,23 +6,45 @@ class OwnProblem extends React.Component {
   constructor(props) {
     super(props);
   }
+  handlePreview(event) {
+    event.preventDefault();
+    const preview_url = `problems/${this.props.problem.uuid}/pdf/`;
+    fetch(preview_url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.blob())
+      .then((blob) => {
+        const blob_url = URL.createObjectURL(blob);
+        window.open(blob_url);
+      });
+  }
   render() {
-    const url = "/problems/edit/" + this.props.problem.uuid;
+    const edit_url = "/problems/edit/" + this.props.problem.uuid;
+    const actions = this.props.problem.editable ? (
+      <>
+        <Link to={edit_url}>&#x270E;</Link>
+        <a href="#Eliminar">&#x1F5D1;</a>{" "}
+      </>
+    ) : (
+      "No hay acciones disponibles"
+    );
     return (
       <tr>
         <td width="25%">
           {" "}
-          <a href="#VerProblema">{this.props.problem.name}</a>
+          <a href="#" onClick={this.handlePreview}>
+            {this.props.problem.name}
+          </a>
         </td>
         <td width="25%"> {this.props.problem.created_at}</td>
         <td width="25%">
           {" "}
           <Topic topics={this.props.problem.topics} />{" "}
         </td>
-        <td width="25%">
-          <Link to={url}>&#x270E;</Link>
-          <a href="#Eliminar">&#x1F5D1;</a>
-        </td>
+        <td width="25%">{actions}</td>
       </tr>
     );
   }
--- frontend/src/components/Problem_list/Problem.js
+++ frontend/src/components/Problem_list/Problem.js
@@ -4,13 +4,32 @@ import Topic from "./Topic";
 class Problem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.getPreview = this.getPreview.bind(this);
+  }
+  getPreview(event) {
+    event.preventDefault();
+    const url = `/api/problems/${this.props.problem.uuid}/pdf/`;
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.blob())
+      .then((blob) => {
+        let preview_url = URL.createObjectURL(blob);
+        window.open(preview_url);
+      });
   }
   render() {
     return (
       <tr>
         <td width="25%">
           {" "}
-          <a href="#VerProblema">{this.props.problem.name}</a>
+          <a href="#" onClick={this.getPreview}>
+            {this.props.problem.name}
+          </a>
         </td>
         <td width="25%"> {this.props.problem.created_at}</td>
         <td width="25%">
