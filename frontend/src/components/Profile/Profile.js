6e9fb6ad60c52de60405577f90eef5d74f33563d
Erick Lemus <elemusf@gmail.com>
2020-07-12 19:18:01 -0400

    feat(frontend): Create Profile Component

---
+++ frontend/src/components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Este es el perfil</h1>;
+  }
+}
+
+export default Profile;
