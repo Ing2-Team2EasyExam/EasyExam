cd408f93c5fa5e2dcc3689fee2df3a81e57f58be
csepulvedaa <mess@LAPTOP-1HNHOLB1.localdomain>
2020-07-12 23:41:43 -0400

    Admin User Creation Form Done and working

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -1,22 +1,39 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-
-# from apps.challenge.models import Criterion, Challenge
 from apps.exam.admin import ProblemAdmin, ExamAdmin
 from apps.exam.models import Problem, Topic, Exam
 from apps.user.models import User, Transaction
 
-User.list_display = ("first_name", "last_name", "email", "is_active")
 
-admin.site.register(User)
+# Definimos la clase admin
+
+# Campos para creación y edición de un usuario
+# Correo electronico
+# Usuario activo
+# Avatar / Foto de perfil
+# Contraseña
+# Grupo de Usuario
+# Permisos TODO ver diferencias entre is_staff y is_superuser, si es staff puede entrar al admin de django, es equivalente
+# al campo is_admin del modelo de datos
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Correo Electrónico ", {"fields": ["email"]}),
+        ("Contraseña", {"fields": ["password"]}),
+        ("Nombre Completo", {"fields": ["first_name", "last_name"]}),
+        ("Foto De Perfil", {"fields": ["image"]}),
+        ("Grupos", {"fields": ["groups"]}),
+        ("Usuario Activo", {"fields": ["is_active"]}),
+    ]
+
+
+# Registramos la clase admin con su modelo asociado
+admin.site.register(User, UserAdmin)
+
+# Registramos la clase problema con su modelo asociado
 admin.site.register(Exam, ExamAdmin)
 admin.site.register(Problem, ProblemAdmin)
-# admin.site.register(Image)
+
+# Registramos la clase Topicos
 admin.site.register(Topic)
-# admin.site.register(Criterion)
-# admin.site.register(Challenge)
-# admin.site.register(Vote)
-# admin.site.register(CurrentChallenge)
-# admin.site.register(Transaction)
-# admin.site.unregister(Group)
-# Comment this for now
