d2750f9c9a92ed77bb9ab745222770b0fb1f4ce0
Erick Lemus <elemusf@gmail.com>
2020-07-18 17:14:50 -0400

    feat(frontend): Get basic information on profile page

--- frontend/src/components/Profile/ProfileForm.js
+++ frontend/src/components/Profile/ProfileForm.js
@@ -4,9 +4,45 @@ import { Row, Col, Form, Button } from "react-bootstrap";
 class ProfileForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoaded: false,
+      isLoading: false,
+      first_name: "",
+      last_name: "",
+      email: "",
+    };
+  }
+
+  componentDidMount() {
+    const url = "/api/users/account/";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          this.setState({
+            isLoaded: true,
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: data.email,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
   }
 
   render() {
+    console.log(this.state);
     return (
       <Form>
         <Form.Group as={Row} controlId={this.props.controlId}>
@@ -17,6 +53,7 @@ class ProfileForm extends React.Component {
             <Form.Control
               name={"MyName"}
               type={"text"}
+              value={this.state.first_name}
               placeholder={"Ingresa tu nombre"}
             />
           </Col>
@@ -29,16 +66,9 @@ class ProfileForm extends React.Component {
             <Form.Control
               name={"MyLastname"}
               type={"text"}
+              value={this.state.last_name}
               placeholder={"Ingresa tu apellido"}
             />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId={this.props.controlId}>
-          <Form.Label column sm={3}>
-            {"Se unió el: "}
-          </Form.Label>
-          <Col sm={9}>
-            <Form.Control type={"text"} value={"2020/04/24"} readOnly />
           </Col>
         </Form.Group>
         <Form.Group as={Row}>
