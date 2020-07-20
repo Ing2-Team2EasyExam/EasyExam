8246f983c8d4f800fd811c9c8ee7a0b028a2e321
csepulvedaa <mess@LAPTOP-1HNHOLB1.localdomain>
2020-07-20 00:44:17 -0400

    Added admin button on profile interface

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -3,10 +3,43 @@ import CardForm from "../EEComponents/CardForm";
 import ProfileForm from "./ProfileForm";
 import { PersonCircle } from "react-bootstrap-icons";
 import { Row, Col, Button } from "react-bootstrap";
+import AdminForward from "./AdminForward";
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      is_admin: null,
+      first_name: "1",
+      last_name: "2",
+    };
+  }
+
+  componentDidMount() {
+    const url = "/api/users/account/";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          this.setState({
+            first_name: data.first_name,
+            last_name: data.last_name,
+            is_admin: data.is_admin,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
   }
 
   render() {
@@ -23,12 +56,16 @@ class Profile extends React.Component {
         <Row>
           <Button
             href="/profile/change-password"
-            variant="primary"
+            variant="warning"
             size="lg"
             block
+            style={{ marginTop: "5px" }}
           >
             Cambiar Contraseña
           </Button>
+        </Row>
+        <Row>
+          <AdminForward isAdmin={this.state.is_admin} />
         </Row>
       </CardForm>
     );
